a282b1b7071078c75db0b910dd1373366d7077ca
Charlie <c.brekke@gwmail.gwu.edu>
2026-08-24 21:09:56 -0400

    practice problems

--- lab0/practice/addition_practice.py
+++ lab0/practice/addition_practice.py
@@ -5,10 +5,13 @@ def main():
     a = 12
     b = 4
 
+
+
     # TODO: Add a and b, and store the answer in a variable named result.
-    result = None
+    result = a + b
 
     # TODO: Print result.
+    print(result)
 
 
 if __name__ == "__main__":
--- lab0/practice/fibonacci_exercise.py
+++ lab0/practice/fibonacci_exercise.py
@@ -6,8 +6,13 @@ def fibonacci(n):
         raise ValueError("Fibonacci is not defined for negative integers")
 
     # TODO: Add the base case. Note: Fib(0) is 0; Fib(1) is 1; Fib(2) is 1, so on.
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
 
     # TODO: Return fibonacci: 
+    return fibonacci(n - 1) + fibonacci(n - 2)
     raise NotImplementedError("Complete fibonacci")
 
 
@@ -17,9 +22,10 @@ def main():
     print(f"Fib(20) = {fibonacci(20)}")  # Expected: 6765
     
     # See what happens when you try this:
-    # print(f"Fib(50) = {fibonacci(50)}")  # Expected: 12,586,269,025 (Python can handle this number)
+    print(f"Fib(50) = {fibonacci(50)}")  # Expected: 12,586,269,025 (Python can handle this number)
     
     # Why does it happen? What is the time complexity of this implementation? How can we improve it?
+    # it is a very large number so it takes longer to compute. time complexity is O(n^2)
 
 
 if __name__ == "__main__":
